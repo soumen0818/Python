6415f68fd446c20ab42a46af1fe714015c6d5643
soumen0818 <sdas721444@gmail.com>
2025-06-25 11:57:18 +0530

    Variable

---
+++ variable.py
@@ -0,0 +1,41 @@
+# variable = A container for a value (string, integer, float, boolean).
+#            A varibale behaves as if it was the value it contains .
+
+# String
+first_name = "Soumen"
+food = "Briyani"
+email = "sdas72144@gmail.com"
+
+# print( f"Heloo {first_name}") 
+# print(first_name)  
+# print(f"your mail is : {email}") 
+
+
+# ...............................
+
+# Integers
+age = 25
+quantity = 5 
+num_of_students =50
+
+# print(f"You are {age} years old")
+# print(f"you have {quantity} iteams")   
+# print(f"you have {num_of_students} no. of students")    
+
+# .................................
+
+# Float
+price = 50.5
+distance = 11.6
+cgpa = 8.5
+
+# Boolean => True and false
+is_student = True
+is_student = False
+
+# if is_student:
+#     print(f"you are a student")
+# else:
+#     print(f"you are not student")    
+
+
