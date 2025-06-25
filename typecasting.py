2cf1425ed9f71949edb089226bb8e1a430a04295
soumen0818 <sdas721444@gmail.com>
2025-06-25 20:45:54 +0530

    TypeCasting

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+# TypeCasting = The Process of converting a variable from one data type to another.
+# Python provides several built-in functions to facilitate casting, including int(), float() and str() among others.
+
+
+
+name = "soumen"
+age = 20
+gpa = 8.2
+is_students = True
+
+# print(type(variable_name))
+
+# gpa = int(gpa)
+
+age = str(age)
+
+# age += 2   => can only concatenate str (not "int") to str
+
+# age += "5"  => Right ,because now 5 is string not number
+
+print(age)
