81546003fcf43cff25153081a7675bde5da48ba2
soumen0818 <sdas721444@gmail.com>
2025-06-26 11:11:36 +0530

    Math Function

--- Operators&mathfunction.py
+++ Operators&mathfunction.py
@@ -26,4 +26,27 @@ Book = 10
 # Book = Book % 4
 # Book %= 4
 
-print(Book)
+# print(Book)
+
+# Math function .................................................
+
+
+x = 2.54
+y = -5
+z = 6
+
+# result = round(x)      => The round() function in Python is a built-in function used to round a number to
+# a specified number of decimal places or to the nearest integer.    // output = 3
+
+# result = abs(y)      => it is use for convert negative to positive   // output = 5
+
+# result = pow (6, 2)   => power function  // Output = 36
+
+# result = max (x, y, z)   => Find maximum value among the diffrent variable    //output = 6
+# result = min (x, y, z)   => Find maximum value among the diffrent variable    // output = -5
+
+# print(result) 
+
+
+#...........................................................................
+
