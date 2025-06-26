702fe49a6dc440e3e9d41363996ea03cf1933cdb
soumen0818 <sdas721444@gmail.com>
2025-06-26 09:28:42 +0530

    User Input

--- userinput.py
+++ userinput.py
@@ -1,12 +1,40 @@
 # input() = A function that prompts the user to enter data 
 #           Returns the entered data as a string
 
-name = input ("What is you name ? : ")
-age = int (input ("how old are you ? : "))
+# name = input ("What is you name ? : ")
+# age = int (input ("how old are you ? : "))
 
 # age = int (age)
-age = age + 2
+# age = age + 2
 
-print(f"your name is {name}")
-print("Happy BirthDay !")
-print(f"You are {age} year old")
+# print(f"your name is {name}")
+# print("Happy BirthDay !")
+# print(f"You are {age} year old")
+
+#......................................................
+
+
+# Exercise 1 => Rectangle area cal.
+#               Area = length * width
+
+
+# length = float (input("Enter the length : "))
+# width = float (input("Enter the width : "))
+
+# Area = length *width
+
+# print(f"Area of the rectangle is: {Area}cm²")
+
+
+#........................................................
+
+
+# Exercise 2 => shoping cart program
+
+item = input("What item would you like to buy? : ")
+price = float(input("What is the price of the iteam?: "))
+Quantity = int(input("how many would you like to buy: "))
+
+total_price = price * Quantity
+
+print(f"Your total bill is : ${total_price}")
