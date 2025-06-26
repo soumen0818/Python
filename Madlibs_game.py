8ccf76a7d70d4f5a7e3320f19ab192d30d9d2e17
soumen0818 <sdas721444@gmail.com>
2025-06-26 09:51:29 +0530

    Madlibs game

---
+++ Madlibs_game.py
@@ -0,0 +1,12 @@
+# Madlibs game => where you create a story by filling in blanks with random words.
+
+adjective1 = input("Enter an adjective (Description): ")
+adjective2 = input("Enter an adjective (Description): ")
+adjective3 = input("Enter an adjective (Description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+
+
+print(f"Today i went to a {adjective1} place")
+print(f"In the place i saw my favorite person {noun1}")
+print(f"{noun1} was a very {adjective2} person")
+print(f"I was so {adjective3}!")
