983a45bd903df1378440717154aa586b50c8b4cf
soumen0818 <sdas721444@gmail.com>
2025-06-26 09:07:39 +0530

    User input

---
+++ userinput.py
@@ -0,0 +1,12 @@
+# input() = A function that prompts the user to enter data 
+#           Returns the entered data as a string
+
+name = input ("What is you name ? : ")
+age = int (input ("how old are you ? : "))
+
+# age = int (age)
+age = age + 2
+
+print(f"your name is {name}")
+print("Happy BirthDay !")
+print(f"You are {age} year old")
