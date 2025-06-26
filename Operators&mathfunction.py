3ef18044bc5470aee141e4430a236b75d5a8e0da
soumen0818 <sdas721444@gmail.com>
2025-06-26 10:46:38 +0530

    Operators

---
+++ Operators&mathfunction.py
@@ -0,0 +1,29 @@
+# Operators => Operators in general are used to perform operations on values and variables.
+
+Book = 10
+
+# Addition ...........
+# Book = Book + 5
+# Book += 5
+
+# Subtraction..........
+# Book = Book - 4
+# Book -= 5
+
+# Multipication.........
+# Book = Book * 2
+# Book *= 2
+
+# Division..............
+# Book = Book/2
+# Book /= 2 
+
+# Squar ..............(Exponentiation)
+# Book = Book ** 2
+# Book **= 2
+
+# Reminder ...........(Modulus)
+# Book = Book % 4
+# Book %= 4
+
+print(Book)
