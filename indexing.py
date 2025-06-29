d1459b0a6aeccee978c09564b9cbd1775b9f11a5
soumen0818 <sdas721444@gmail.com>
2025-06-29 13:01:16 +0530

    indexing in python

---
+++ indexing.py
@@ -0,0 +1,16 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            represent => [start : end : step]
+
+credit_card_number = "1542-8574-9632-4581"
+
+last_digit = (credit_card_number[-4:])
+
+# print(credit_card_number[0])
+# print(credit_card_number[1:4])
+# print(credit_card_number[:4])
+# print(credit_card_number[5:9])
+# print(credit_card_number[5:])
+# print(credit_card_number[-5])
+# print(credit_card_number[::3])
+
+print(f"xxxx-xxxx-xxxx-{last_digit}")
