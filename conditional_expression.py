fe1c0de1f852363fad027dde5709fa2e9c5e16ee
soumen0818 <sdas721444@gmail.com>
2025-06-29 08:51:55 +0530

    Conditional Expression

---
+++ conditional_expression.py
@@ -0,0 +1,21 @@
+# Conditional Expression  => A one line short-cut for the if-else statement (ternary operator)
+#                            print or assign one of two values based on a condition .
+#                           expression => x if condition else y
+
+num = -10
+
+a = 6
+b = 8
+age = 20
+temp = 35
+
+# print("positive" if num > 0 else "Negative")
+# result = "Even" if num % 2 == 0 else "ODD"
+# max_num = a if a>b else b
+# print ("you are adult" if age >= 18 else "You are not adult")
+# print ("hot" if temp >= 20 else "cold")
+
+
+print()
+
+
