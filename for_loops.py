c709e4521bc2091146636c1fd6b33721d1f98fb4
soumen0818 <sdas721444@gmail.com>
2025-06-30 21:13:11 +0530

    For looop

---
+++ for_loops.py
@@ -0,0 +1,27 @@
+# for looop = Execute a block of code a fixed number of times.
+#             you can iterate over a range , string, sequence, etc.
+
+# for x in range (1,11):
+#     print(x)
+
+# 1. Python For Loop with String  
+
+# name = "soumen"
+# for x in name :
+#     print(x)
+    
+# 2. Using range() with For Loop  => range(start, stop, step): Generates numbers from start to stop-1, incrementing by step.
+
+# for x in range (1,20,2):
+#     print(x)
+
+# 3. Continue & Break with For Loop
+
+phone_number = "9339982624"
+for x in phone_number :
+    if x == "8":
+        # continue 
+        break  
+    print(x)
+
+
