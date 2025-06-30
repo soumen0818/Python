d8eee473e96422aad66dc0c8f031f99211367925
soumen0818 <sdas721444@gmail.com>
2025-06-30 21:14:11 +0530

    For loop in python

--- for_loops.py
+++ for_loops.py
@@ -1,19 +1,19 @@
 # for looop = Execute a block of code a fixed number of times.
 #             you can iterate over a range , string, sequence, etc.
 
-# for x in range (1,11):
-#     print(x)
+for x in range (1,11):
+    print(x)
 
 # 1. Python For Loop with String  
 
-# name = "soumen"
-# for x in name :
-#     print(x)
+name = "soumen"
+for x in name :
+    print(x)
     
 # 2. Using range() with For Loop  => range(start, stop, step): Generates numbers from start to stop-1, incrementing by step.
 
-# for x in range (1,20,2):
-#     print(x)
+for x in range (1,20,2):
+    print(x)
 
 # 3. Continue & Break with For Loop
 
