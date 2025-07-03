593c26e23daa8fb5c41fafd5db3ac3964cced713
soumen0818 <sdas721444@gmail.com>
2025-07-03 22:15:45 +0530

    Dictionary in python

--- dictionaries.py
+++ dictionaries.py
@@ -21,4 +21,20 @@ capitals ={
 # capitals.update({"Germany": "Barlin"})    ==> Using ubdate method , we can insert a key value pair or ubdate.
 # capitals.update({"India":"Kolkata"})
 # capitals.pop("Russia")        ==> remove specified key and return the corresponding value.
-print(capitals)
+# capitals.popitem()            ==> Remove the latest added key value
+# capitals.clear()
+
+# key = capitals.keys()     ==> Return a set-like object providing a view on the dict's keys.
+# for key in capitals.keys():
+#     print(key)
+
+# values = capitals.values()   ==> Return an object providing a view on the dict's values.
+# for value in capitals.values():
+#     print(value)
+
+# items = capitals.items()     ==> Return a set-like object providing a view on the dict's items.
+# for key, value in capitals.items():
+#     print(f"{key} : {value}")
+
+
+# print(capitals)
