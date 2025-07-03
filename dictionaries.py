4bff6e1bd0438846c7d0d022dbadc7aac903865c
soumen0818 <sdas721444@gmail.com>
2025-07-03 22:00:51 +0530

    Dictionary

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+# Dictionary ==> A collection of {key:value} pairs.
+#                ordered and changeable, No duplicates
+
+
+capitals ={
+    "USA": "washington D.C",
+    "India": "New Delhi",
+    "Russia": "Moscow"
+}
+
+# print(dir(capitals))
+# print (help(capitals))
+# print(capitals.get("USA"))
+
+# if capitals.get("japan"):
+#     print("The capital is exist")
+# else :
+#     print("The capitals is not exist")
+
+
+# capitals.update({"Germany": "Barlin"})    ==> Using ubdate method , we can insert a key value pair or ubdate.
+# capitals.update({"India":"Kolkata"})
+# capitals.pop("Russia")        ==> remove specified key and return the corresponding value.
+print(capitals)
