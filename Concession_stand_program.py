a5186e563d3ac00552715822686185d4f7728fd1
soumen0818 <sdas721444@gmail.com>
2025-07-05 23:41:59 +0530

    Exercise of Dictionary

--- Concession_stand_program.py
+++ Concession_stand_program.py
@@ -18,4 +18,19 @@ for key, value in menu.items():
 
 print("-----------------------------------")
 
+while True:
+    food = input("Select an item (q to quit): ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+        
+print("-----------YOUR ORDER-------------")
+        
+for food in cart :
+    total += menu.get(food) 
+    print(food, end = " ")  
     
+print()
+print(f"Total is : ${total:.2f}")
+     
