fc538339833b25aa8c7922f4f14cf36d36d2d8c5
soumen0818 <sdas721444@gmail.com>
2025-07-06 20:59:13 +0530

    Function in Python

--- function.py
+++ function.py
@@ -16,13 +16,22 @@
 
 # Example 2..........................
 
-def display_invoice(username, amount, due_date):
-    print(f"Hello {username}!")
-    print(f"Your bill of ${amount:.2f} is due : {due_date}")
+# def display_invoice(username, amount, due_date):
+#     print(f"Hello {username}!")
+#     print(f"Your bill of ${amount:.2f} is due : {due_date}")
     
-display_invoice("soumen", 45.254, "01/05/2025")
+# display_invoice("soumen", 45.254, "01/05/2025")
 
 
+# Return ==> statement used to end a function and send a result back to the caller.
 
+def create_name (first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("soumen", "das")
+
+print(full_name)
 
 
