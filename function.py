b828c2c3f453464b08a29519483f69970466b075
soumen0818 <sdas721444@gmail.com>
2025-07-06 20:43:30 +0530

    Example 1

---
+++ function.py
@@ -0,0 +1,13 @@
+# Function = A block of reusable code 
+#            place () after the function name to invoke it.
+
+def happy_birthday(name, age):
+    print(f"Happy Birthday to {name}")
+    print(f"you are {age} years old !")
+    print("Happy Birthday to you 🎈!")
+    print()
+    
+    
+happy_birthday("Soumen", 20)
+happy_birthday("suman", 30)
+happy_birthday("Arka", 40)
