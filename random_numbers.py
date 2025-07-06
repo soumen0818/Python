7e3465a99c28e5eb69d6b4c57b8b26c4d0a37dbd
soumen0818 <sdas721444@gmail.com>
2025-07-06 09:47:51 +0530

    Random Numbers in python

---
+++ random_numbers.py
@@ -0,0 +1,14 @@
+import random
+
+low = 1
+high = 50
+options = ("rock", "paper", "scissorc")
+cards = ["2", "3","4","5","6","7","8","9","J","K","Q","A"]
+
+
+# number = random.randint(low, high)    ==> Return random integer in range [a, b], including both end points.
+# number = random.random()
+# option = random.choice(options)       ==> Choose a random element from a non-empty sequence.
+# random.shuffle(cards)                 ==> Shuffle list x in place, and return None.
+
+print(cards)
