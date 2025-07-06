df391acac0441c554fc28ff337ec2c09a32b021b
soumen0818 <sdas721444@gmail.com>
2025-07-06 20:52:09 +0530

    Example 2

--- function.py
+++ function.py
@@ -1,13 +1,28 @@
 # Function = A block of reusable code 
 #            place () after the function name to invoke it.
 
-def happy_birthday(name, age):
-    print(f"Happy Birthday to {name}")
-    print(f"you are {age} years old !")
-    print("Happy Birthday to you 🎈!")
-    print()
+# Example 1......................
+
+# def happy_birthday(name, age):
+#     print(f"Happy Birthday to {name}")
+#     print(f"you are {age} years old !")
+#     print("Happy Birthday to you 🎈!")
+#     print()
     
     
-happy_birthday("Soumen", 20)
-happy_birthday("suman", 30)
-happy_birthday("Arka", 40)
+# happy_birthday("Soumen", 20)
+# happy_birthday("suman", 30)
+# happy_birthday("Arka", 40)
+
+# Example 2..........................
+
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}!")
+    print(f"Your bill of ${amount:.2f} is due : {due_date}")
+    
+display_invoice("soumen", 45.254, "01/05/2025")
+
+
+
+
+
