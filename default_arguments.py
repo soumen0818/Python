c45e1a0f58c03778f7f5f72b971bf9900538c506
soumen0818 <sdas721444@gmail.com>
2025-07-06 21:24:31 +0530

    Default Arguments in python

--- default_arguments.py
+++ default_arguments.py
@@ -8,4 +8,17 @@ def net_price (list_price, discount = 0, tax = 0.05):
 
 # print(net_price(500))
 # print(net_price(500, 0.1))
-print(net_price(500, 0.1, 0))
+# print(net_price(500, 0.1, 0))
+
+
+# Example ...........
+
+import time
+
+def count (start, end):
+    for x in range (start, end+1):
+        print(x)
+        time.sleep(0.1)
+    print("DONE!")
+    
+count(0,100)
