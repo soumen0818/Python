2653610003b7832e4cc7cb3c84e90747287ee549
soumen0818 <sdas721444@gmail.com>
2025-07-14 23:55:23 +0530

    Theory

---
+++ 38_if_statement.py
@@ -0,0 +1,5 @@
+# if_name_ == _main_:(This script can be import or standalone)
+#             Function and classes in this module can be reused without the main block of code executing .
+
+# ex.library = import libary for functionality, when runing libary directly, display a help page.
+
