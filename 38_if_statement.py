8fab617bb770ef68277c1920d213d798d745e737
soumen0818 <sdas721444@gmail.com>
2025-07-15 23:32:53 +0530

    if statemnent in python

--- 38_if_statement.py
+++ 38_if_statement.py
@@ -1,5 +1,7 @@
-# if_name_ == _main_:(This script can be import or standalone)
+# if__name__ == __main__:(This script can be import or standalone)
 #             Function and classes in this module can be reused without the main block of code executing .
 
 # ex.library = import libary for functionality, when runing libary directly, display a help page.
 
+
+    
