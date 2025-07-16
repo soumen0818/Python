5a8a8aaa8ee4d9721a4888615ea8803ed38300b1
soumen0818 <sdas721444@gmail.com>
2025-07-16 11:07:12 +0530

    python slod machine game

--- 40_slot_machine_game.py
+++ 40_slot_machine_game.py
@@ -2,13 +2,28 @@
 import random
 
 def spin_row():
-    pass
+    symbols = ['🍒', '🍉', '🌶', '🥟', '🎇']
+    
+    return[random.choice(symbols) for _ in range(3)]
 
-def print_row():
-    pass
+def print_row(row):
+    print("************")
+    print(" | ".join(row))
+    print("************")
 
-def get_payout():
-    pass
+def get_payout(row, bet):
+    if row[0] == row[1] == row[2]:
+        if row[0] =='🍒':
+            return bet * 3
+        if row[0] == '🍉':
+            return bet * 4
+        if row[0] == '🌶':
+            return bet * 5
+        if row[0] == '🥟':
+            return bet * 10
+        if row[0] == '🎇':
+            return bet * 20
+    return 0
 
 def main():
     balance = 100
@@ -21,11 +36,14 @@ def main():
     while balance > 0:
         print(f"Your current Balance is ${balance}")
         
-        bet = int(input("Enter your Bet amount: "))
+        bet = input("Enter your Bet amount: ")
         
         if not bet.isdigit():
             print("Please enter a valid number")
             continue
+        
+        bet = int(bet)
+        
         if bet > balance:
             print("Insufficient Balanace!")
             continue
@@ -33,9 +51,28 @@ def main():
             print("Bet must be greater than 0")
             continue
         
+        
         balance -= bet
         
-
+        row = spin_row()
+        print("spinning..\n")
+        print_row(row)
+        payout = get_payout(row,bet)
+        
+        if payout > 0:
+            print(f"You won ${payout}")
+        else:
+            print("Sorry! you lost the game..")
+            
+        balance += payout
+        
+        play_again = input("Do you want to play again? (Y/N): ").upper()
+        
+        if play_again != 'Y':
+            break
+        
+    print(f"Game over! Your final balance is ${balance}")
+        
 
 if __name__ == '__main__':
     main()
